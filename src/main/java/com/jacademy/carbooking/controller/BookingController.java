ecb39b382500742841a652cecc464633af70f7aa
Jainendra Kumar <M1049242@mindtree.com>
2020-01-26 11:46:20 +0530

    basic swagger implemented for restapi documentation

--- src/main/java/com/jacademy/carbooking/controller/BookingController.java
+++ src/main/java/com/jacademy/carbooking/controller/BookingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +17,17 @@ import com.jacademy.carbooking.entity.Booking;
 import com.jacademy.carbooking.service.BookingService;
 
 @RestController
+@RequestMapping("/booking")
 public class BookingController {
 	@Autowired
 	BookingService bookingService;
 
-	@GetMapping("/bookings")
+	@GetMapping("/")
 	public ResponseEntity<List<Booking>> findAllBookings() {
 		return new ResponseEntity<>(bookingService.findAllBookings(), HttpStatus.OK);
 	}
 
-	@GetMapping("/booking/{bookingId}")
+	@GetMapping("/{bookingId}")
 	public ResponseEntity<Booking> findByBookingId(@PathVariable Integer bookingId) {
 		return new ResponseEntity<>(bookingService.findByBookingId(bookingId), HttpStatus.OK);
 	}
--- src/main/java/com/jacademy/carbooking/controller/CustomerController.java
+++ src/main/java/com/jacademy/carbooking/controller/CustomerController.java
@@ -9,12 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jacademy.carbooking.entity.Customer;
 import com.jacademy.carbooking.service.CustomerService;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 	@Autowired
 	CustomerService customerService;
@@ -25,12 +27,12 @@ public class CustomerController {
 		return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
 
 	}
-	@GetMapping("/customers")
+	@GetMapping("/")
 	public ResponseEntity<List<Customer>> findAllCustomers(){
 		List<Customer> customers = customerService.findAllCustomers();
 		return new ResponseEntity<>(customers, HttpStatus.OK);
 	}
-	@GetMapping("/customer/{customerId}")
+	@GetMapping("/{customerId}")
 	public ResponseEntity<Customer> findByCustomerId(@PathVariable Integer customerId){
 		Customer customer = customerService.findByCustomerId(customerId);
 		return new ResponseEntity<>(customer, HttpStatus.OK);
